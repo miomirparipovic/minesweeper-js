db213a29e014511eca5ed3e454fa70c4e534b263
Miomir Paripovic <deploy.friday@gmail.com>
2024-05-16 13:10:30 +0200

    Add basic grid, hardcoded

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
 import "./style.css";
+
+function generateBoard() {
+  const board = [];
+
+  for (let x = 0; x < 10; x++) {
+    const row = [];
+
+    for (let y = 0; y < 10; y++) {
+      const element = document.createElement("div");
+      element.dataset.status = "hidden";
+
+      const tile = {
+        element,
+        x,
+        y,
+      };
+
+      row.push(tile);
+    }
+
+    board.push(row);
+  }
+
+  //   console.log("board", board);
+  return board;
+}
+
+const board = generateBoard();
+const boardElement = document.querySelector(".board");
+board.forEach((row) => {
+  row.forEach((tile) => {
+    boardElement.append(tile.element);
+  });
+});
